68228d502755084e7c15b6b678f449f592eef186
murakami20220204 <98999589+murakami20220204@users.noreply.github.com>
2024-12-20 23:20:18 +0900

    CoCreateInstance に関する問題を修正

--- Paint/PaintLib/Include/hello.h
+++ Paint/PaintLib/Include/hello.h
@@ -6,13 +6,16 @@ Copyright 2024 Taichi Murakami.
 #include <windows.h>
 
 /* Library Identifier */
-EXTERN_C const IID LIBID_PaintLibrary;
+//EXTERN_C const IID LIBID_PaintLibrary;
+const IID DECLSPEC_SELECTANY LIBID_PaintLibrary = { 0x7BE73173, 0x79FB, 0x4F30, 0x91, 0xF0, 0x6B, 0xDC, 0x26, 0xE0, 0xC6, 0x25 };
 
 /* Interface Identifiers */
-EXTERN_C const IID IID_IHelloWorld;
+//EXTERN_C const IID IID_IHelloWorld;
+const IID DECLSPEC_SELECTANY IID_IHelloWorld = { 0x6E6803DC, 0x26C5, 0x483A, 0x9A, 0x8B, 0x6A, 0xC8, 0xD9, 0xF6, 0x88, 0x52 };
 
 /* Class Identifiers */
-EXTERN_C const CLSID CLSID_HelloWorld;
+//EXTERN_C const CLSID CLSID_HelloWorld;
+const CLSID DECLSPEC_SELECTANY CLSID_HelloWorld = { 0xCBE40A9F, 0xCE73, 0x46C8, 0xAD, 0xC4, 0x17, 0xCE, 0x59, 0x2B, 0x76, 0x5C };
 
 #undef INTERFACE
 #define INTERFACE IHelloWorld
